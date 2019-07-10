7ab253aec8fe9904a39f950bfbd4c936fec247fc
나랑 <nalang0987@naver.com>
2019-07-10 16:57:18 +0900

    vue todo update

--- todo_20190709/src/App.vue
+++ todo_20190709/src/App.vue
@@ -1,28 +1,82 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="col-md-6 offset-md-3">
+        <h1 class="text-center mb-4">Todo List</h1>
+        <input type="text" class="form-control mb-4" v-model="userInput" @keyup.enter="addNewTodo">
+
+        <div class="list-group mb-4">
+          <ul class="nav nav-tabs">
+            <li class="nav-item">
+              <a class="nav-link active" data-toggle="tab" href="#" @click="changeCurrentState('active')">할일</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="tab" href="#" @click="changeCurrentState('done')">완료</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="tab" href="#" @click="changeCurrentState('all')">전체</a>
+            </li>
+          </ul>
+        </div>
+
+
+
+        <div class="list-group mb-4">
+          <template v-for="todo in actionTodoList">
+            <button type="button" class="list-group-item text-left" @click="toggleState(todo)">{{todo.label}}</button>
+          </template>
+        </div>
+
+        <div class="text-right">
+          <button type="button" class="btn btn-sm" @click="changeCurrentState('active')">할일</button>
+          <button type="button" class="btn btn-sm" @click="changeCurrentState('done')">완료</button>
+          <button type="button" class="btn btn-sm" @click="changeCurrentState('all')">전체</button>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    name : 'app',
+    data(){
+      return {
+        userInput : '',
+        todo : [],
+        currentState : 'active',
+      }
+    },
+    computed : {
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+      actionTodoList(){
+        return this.todo.filter(todo => todo.state === this.currentState || this.currentState === 'all');
+      }
+
+    },
+    methods : {
+      addNewTodo(){
+        this.todo.push({
+          label : this.userInput,
+          state : 'active',
+        });
+        this.userInput = '';
+      },
+      changeCurrentState(state){
+        this.currentState = state;
+        return false;
+      },
+      toggleState(todo){
+        todo.state = todo.state === 'active'?'done':'active';
+      }
+    }
+  };
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family:'Avenir', Helvetica, Arial, sans-serif;
+    text-align:center;
+    color:#2c3e50;
+    margin-top:30px;
+  }
 </style>
